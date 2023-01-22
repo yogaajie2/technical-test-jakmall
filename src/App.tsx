3e889d2a8ed7853c70e9201a0feb0592ace060c4
yogaajie2 <yogaajie2@gmail.com>
2023-01-22 12:49:21 +0700

    feat: setup App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import styled from 'styled-components';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Page = styled.main`
+  border-radius: 4px;
+  margin-left: 50px;
+  margin-right: 50px;
+  margin-top: 55px;
+  padding-bottom: 20px;
+  padding-left: 40px;
+  padding-right: 20px;
+  padding-top: 30px;
+  background-color: #FFF;
+  box-shadow: 2px 10px 20px rgba(255, 138, 0, 0.1);
+`;
+
+const Back = styled.button`
+  border: none;
+  font-weight: 500;
+  background-color: transparent;
+  opacity: 0.6;
+`;
+
+const StepWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto 25%;
+`;
+
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +47,15 @@ function App() {
         </a>
       </header>
     </div>
+
+      <Page>
+        <Back>Back to cart</Back>
+
+        <StepWrapper>
+          <h1>Delivery details</h1>
+        </StepWrapper>
+      </Page>
+    </>
   );
 }
 
