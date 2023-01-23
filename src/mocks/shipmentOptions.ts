467f6a79fd2499347f4b5bb4da4eba7275b80d91
yogaajie2 <yogaajie2@gmail.com>
2023-01-23 15:17:21 +0700

    refactor: change price to amount

--- src/mocks/shipmentOptions.ts
+++ src/mocks/shipmentOptions.ts
@@ -1,19 +1,19 @@
 const shipmentOptions = [
   {
     provider: 'GO-SEND',
-    price: 15000,
+    amount: 15000,
     estimate: 'today'
   },
 
   {
     provider: 'JNE',
-    price: 9000,
+    amount: 9000,
     estimate: '2 days'
   },
 
   {
     provider: 'Personal Courier',
-    price: 29000,
+    amount: 29000,
     estimate: '1 day'
   }
 ];
