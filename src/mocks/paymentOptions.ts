0cc1c7215e9744c5a31cf8f2d12b2742898f9b52
yogaajie2 <yogaajie2@gmail.com>
2023-01-23 15:16:20 +0700

    feat: add mock paymentOptions

---
+++ src/mocks/paymentOptions.ts
@@ -0,0 +1,16 @@
+const paymentOptions = [
+  {
+    provider: 'e-Wallet',
+    amount: 1500000
+  },
+
+  {
+    provider: 'Bank Transfer'
+  },
+
+  {
+    provider: 'Virtual Account'
+  }
+];
+
+export default paymentOptions;
