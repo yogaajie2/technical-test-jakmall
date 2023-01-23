4fbeb09b5bbe17387d1ce17dddbc255edd7116ed
yogaajie2 <yogaajie2@gmail.com>
2023-01-23 14:01:05 +0700

    feat: add mock shipmentOptions

---
+++ src/mocks/shipmentOptions.ts
@@ -0,0 +1,21 @@
+const shipmentOptions = [
+  {
+    provider: 'GO-SEND',
+    price: 15000,
+    estimate: 'today'
+  },
+
+  {
+    provider: 'JNE',
+    price: 9000,
+    estimate: '2 days'
+  },
+
+  {
+    provider: 'Personal Courier',
+    price: 29000,
+    estimate: '1 day'
+  }
+];
+
+export default shipmentOptions;
